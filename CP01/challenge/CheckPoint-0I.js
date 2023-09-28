1a3f5a9dfda73afb92308823a7ff4c18c89c4393
YasminMattos <yasmin.mattos.conislla@gmail.com>
2023-09-27 23:26:10 -0500

    añade CP01

--- CP01/challenge/CheckPoint-0I.js
+++ CP01/challenge/CheckPoint-0I.js
@@ -1,52 +1,56 @@
 // Ejercicio 1: Declarar una variable "nombre" y asignarle tu nombre.
 
 // Escribí tu código acá abajo:
-
+const nombre = "Yasmin"
 
 // Ejercicio 2: Declarar una variable "edad" y asignarle tu edad.
 
 // Escribí tu código acá abajo:
-
+const edad = 29
 
 // Ejercicio 3: Declarar una variable "esEstudiante" y asignarle un valor booleano.
 
 // Escribí tu código acá abajo:
-
+const esEstudiante = true
 
 // Ejercicio 4: Declarar una variable "num1" y asignarle un número decimal.
 
 // Escribí tu código acá abajo:
-
+const num1 = 0.17
 
 // Ejercicio 5: Declarar una variable "num2" y asignarle un número entero.
 
 // Escribí tu código acá abajo:
-
+const num2 = 7
 
 // Ejercicio 6: Declarar una variable "mensaje" y asignarle un mensaje de texto.
 
 // Escribí tu código acá abajo:
 
-
+const mensaje = "Hola, si no contesto, deja un mensaje"
 // Ejercicio 7: Declarar una variable "valorNulo" y asignarle el valor null.
 
 // Escribí tu código acá abajo:
-
+const valorNulo = null
 
 // Ejercicio 8: Declarar una variable "numeroIndefinido" sin asignarle valor.
 
 // Escribí tu código acá abajo:
-
+const numeroIndefinido = undefined
 
 // Ejercicio 9: Declarar una variable "frutas" y asignarle un arreglo de nombres de frutas.
 
 // Escribí tu código acá abajo:
-
+const frutas = ["Manzana", "Mango", "Pera", "Higo", "Sandía", "Durazno"]
 
 // Ejercicio 10: Declarar una variable "persona" y asignarle un objeto con propiedades nombre (string), edad(number) y ciudad(string).
 
 // Escribí tu código acá abajo:
-
+const persona = {
+  nombre: "Mike",
+  edad: 22,
+  ciudad: "Lima"
+}
 
 module.exports = {
   nombre,
